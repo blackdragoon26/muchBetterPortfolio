8452d03c06ec4da04245b2d53c585270f915c4a3
Sankalp Jha <sankalp.jha9643@gmail.com>
2025-09-16 17:19:04 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    // ✅ Warning: ignores ESLint errors in builds
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
